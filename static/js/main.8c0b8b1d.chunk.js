62621df241d2cc9ff5cef9c0fd38cef921978f69
Jakub Parlej <jakubparlej@gmail.com>
2021-12-06 17:03:32 +0100

    Updates

---
+++ static/js/main.8c0b8b1d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmanage-landing-page"]=this["webpackJsonpmanage-landing-page"]||[]).push([[0],{11:function(n,e,t){"use strict";t.r(e);var c=t(1),a=t.n(c),i=t(3),r=t.n(i),s=(t(8),t(9),t(0));var o=function(){return Object(s.jsx)("div",{className:"App",children:Object(s.jsx)("h1",{children:"Hello World"})})};r.a.render(Object(s.jsx)(a.a.StrictMode,{children:Object(s.jsx)(o,{})}),document.getElementById("root"))},8:function(n,e,t){},9:function(n,e,t){}},[[11,1,2]]]);
+//# sourceMappingURL=main.8c0b8b1d.chunk.js.map

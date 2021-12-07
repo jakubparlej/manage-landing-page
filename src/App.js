92ad2ed5c4e000cb54e099d64e03c950e14aa32e
Jakub Parlej <jakubparlej@gmail.com>
2021-12-07 15:09:39 +0100

    Added Header section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
 import { Navbar } from "./components";
+import { Header } from "./containers";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
     </div>
   );
 }
---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Button from '../../components/button/Button';
+import image from '../../assets/illustration-intro.svg';
+import bg from '../../assets/bg-tablet-pattern.svg';
+import './Header.scss';
+
+function Header() {
+  return (
+    <div className="header section__padding">
+      <img className="header__bg" src={bg} alt="background"/>
+      <div className="header__content">
+        <h1>Bring everyone together to build better products.</h1>
+        <p>Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goals in view</p>
+        <Button title="Get Started"/>
+      </div>
+
+      <div className="header__image">
+        <img src={image} alt="illustration"/>
+      </div>
+    </div>
+  )
+}
+
+export default Header;

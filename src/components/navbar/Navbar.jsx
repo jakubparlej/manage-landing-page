f3ae45c888f5b72e53e51a686c17394551edc78e
Jakub Parlej <jakubparlej@gmail.com>
2021-12-07 13:35:28 +0100

    Completed Navbar section with mobile menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import logo from "../../assets/logo.svg";
 import "./Navbar.scss";
 import Button from "../button/Button";
@@ -9,7 +10,7 @@ function Navbar() {
   return (
     <div className="navbar section__padding">
       <img src={logo} alt="logo" />
-      <div className="navbar__menu">
+      <div className="navbar__links">
         <a href="#pricing">
           <p>Pricing</p>
         </a>
@@ -26,7 +27,47 @@ function Navbar() {
           <p>Community</p>
         </a>
       </div>
-      <Button title="Get Started" />
+      <div className="navbar__btn">
+        <Button title="Get Started" />
+      </div>
+      <div className="navbar__menu">
+        {toggleMenu ? (
+          <RiCloseLine
+          className="fade-in"
+            color="#000"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+           className="fade-in"
+            color="#000"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="navbar__menu-container fade-in">
+            <div className="navbar__menu-links">
+              <a href="#pricing">
+                <p>Pricing</p>
+              </a>
+              <a href="#product">
+                <p>Product</p>
+              </a>
+              <a href="#about-us">
+                <p>About Us</p>
+              </a>
+              <a href="#careers">
+                <p>Careers</p>
+              </a>
+              <a href="#community">
+                <p>Community</p>
+              </a>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
